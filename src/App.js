7fe50d6565d2d17ebbfd9eeb5d5ff6cebd405105
Prem_sathe <premsathe78@gmail.com>
2024-02-12 16:37:19 +0530

    phone-no

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PhoneNumberValidation from './PhoneNumberValidation';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Phone Number Validation Example</h1>
+      <PhoneNumberValidation />
+    </div>
+  );
+};
+
+export default App;
